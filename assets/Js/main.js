b304a897a72c0b395ed8e2a6d1509395704af2c3
Rico Quintanilla <ricoquin@outlook.com>
2020-02-18 14:46:26 -0600

    updated portfolio

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -1,31 +1,5 @@
-// side nav //
-function openNav() {
-  document.getElementById('.mySidenav').style.width = '250px';
-}
-
-function closeNav() {
-  document.getElementById('.mySidenav').style.width = '0';
-}
-
-// About Me Carosel //
-
-$(document).ready(function() {
-  2;
-  $('.demo').slick();
-  3;
-});
-
-// Carosel Function //
-
-$('.autoplay').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000
-});
-
 // TOP NAV SPACING ANIMATION //
 
-$('a.spacing').on('mouseenter', function() {
-  $(this).toggleClass('spaced');
-});
+// $('a.spacing').on('mouseenter', function() {
+//   $(this).toggleClass('spaced');
+// });
